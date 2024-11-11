e8f2330faf12a798e75bdb9cab51893dab4fe968
Gabriel Andrade <gandradecortez50@gmail.com>
2024-11-11 11:00:53 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,24 @@ if [ "$(id -u)" -ne 0 ]; then
     
 fi
 
+#Nome da aplicação
 APP_NAME=blog
+
+
+
+# Verifica e instala o git
+git --version
+if [ $? -eq o ]; then
+    echo "O Git Compose está instalado!"
+else
+    #Instalando Git
+    sudo chmod +x git_install.sh
+    sudo ./git_install.sh
+fi
 git clone https://github.com/andradesysadmin/$APP_NAME
 cd $APP_NAME/
+
+
 
 #Verificando e instalando Docker Compose
 docker-compose --version
@@ -20,8 +35,8 @@ else
     #Instalando Docker Compose
     sudo curl -SL https://github.com/docker/compose/releases/download/v2.30.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 fi
-
 docker-compose up -d --build
+
 
 
 #Instalando Docker
@@ -32,7 +47,6 @@ else
     sudo chmod +x docker_install.sh
     sudo ./docker_install.sh
 fi
-
 docker rm -f $APP_NAME || true
 docker rmi -f || true
 docker build -t $APP_NAME .
