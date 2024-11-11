f4f0a92e8125eec764c23f01b411fdfbdcd1ba41
Gabriel Andrade <gandradecortez50@gmail.com>
2024-11-11 10:41:12 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# verifica se o script foi executado como root
+if [ "$(id -u)" -ne 0 ]; then
+
+    echo "O script deve ser executado com privilegios de super usuario!"
+
+fi
+
+APP_NAME=blog
+
+git clone https://github.com/andradesysadmin/$APP_NAME
+cd $APP_NAME/
+
+#Verificando e nstalando Docker Compose
+docker-compose --version
+if [ $? -eq o ]; then
+    echo "Docker Compose está instalado!"
+else
+    #Instalando Docker Compose
+    sudo curl -SL https://github.com/docker/compose/releases/download/v2.30.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+fi
+
+docker-compose up -d --build
+
+
+#Instalando Docker
+docker --version
+if [ $? -eq o ]; then
+    echo "Docker está instalado!"
+else
+    sudo chmod +x docker_install.sh
+    sudo ./docker_install.sh
+fi
+
+docker rm -f $APP_NAME || true
+docker rmi -f || true
+docker build -t $APP_NAME .
+docker run -d -p 8000:8000 --name $APP_NAME $APP_NAME
